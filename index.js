255c734dab9a91c569459a6db4d6fbd4504541f4
Melanie Moore <mcmoore@uindy.edu>
2018-05-15 16:05:16 -0400

    Updated homework complete with bonuses

--- index.js
+++ index.js
@@ -3,44 +3,57 @@ const form = document.querySelector('form#userForm')
 const handleSubmit = function(ev) {
   ev.preventDefault()
   
-  const userName = form.userName.value
-  const age = form.age.value
- 
+  renderList()
 
-  const users = document.querySelector('#users')
+  
+}
 
-  const list = document.createElement('ul')
-
-  const nameItem = document.createElement('li')
-  nameItem.textContent = `Name: ${userName}`
-
-  const ageItem = document.createElement('li')
-  ageItem.textContent = `Age: ${age}`
-
-  const colorItem = document.createElement('li')
-  colorItem.textContent = 'Favorite Color: '
 
  function renderColor(){
      //debugger
      const colorDiv = document.createElement('div')
-    const form = ev.target
     const favoriteColor = form.favoriteColor.value
     colorDiv.style.backgroundColor = favoriteColor
     colorDiv.style.width = '6rem'
     colorDiv.style.height = '3rem'
-    colorItem.appendChild(colorDiv)
+    return colorDiv
+    
 }
-renderColor()
+//renderColor()
 
 
-  list.appendChild(nameItem)
-  list.appendChild(ageItem)
-  list.appendChild(colorItem)
 
-  users.appendChild(list)
 
-  //form.reset()
+function renderListItem(list){
+   // debugger
+    const userName = form.userName.value
+const age = form.age.value
+const users = document.querySelector('#users')
+
+    const nameItem = document.createElement('li')
+    nameItem.textContent = `Name: ${userName}`
+  
+    const ageItem = document.createElement('li')
+    ageItem.textContent = `Age: ${age}`
+  
+    const colorItem = document.createElement('li')
+    colorItem.textContent = 'Favorite Color: '
+
+    colorItem.appendChild(renderColor())
+    list.appendChild(nameItem)
+    list.appendChild(ageItem)
+    list.appendChild(colorItem)
+    users.appendChild(list)
+}
+
+  function renderList(){
+    const list = document.createElement('ul')
+   renderListItem(list)
+  }
+ 
+
+  form.reset()
   form.userName.focus()
-}
+
 
 form.addEventListener('submit', handleSubmit)
